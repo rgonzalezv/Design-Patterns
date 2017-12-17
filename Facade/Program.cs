d5e030206df4bb4a30a05cd9b0331f3617168624
Moshe <mmoshikoo@gmail.com>
2017-12-17 10:58:13 +0200

    First commit

---
+++ Facade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccountFacade accessingBank = new BankAccountFacade(12345678, 1234);
+            accessingBank.WithdrawCash(50.00);
+            accessingBank.WithdrawCash(990.00);
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObserverProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+            subject.Attach(new ILS());
+            subject.Attach(new USD());
+
+            subject.Money = 2;
+            subject.Money = 3;
+        }
+    }
+}
