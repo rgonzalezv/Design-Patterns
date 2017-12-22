a49e29ca1c0379fdb09be684e3fb1e53b0049a0c
Moshe <mmoshikoo@gmail.com>
2017-12-22 12:58:29 +0200

    Fixed code

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -7,6 +7,7 @@ namespace Facade
         static void Main(string[] args)
         {
             BankAccountFacade accessingBank = new BankAccountFacade(12345678, 1234);
+            
             accessingBank.WithdrawCash(50.00);
             accessingBank.WithdrawCash(990.00);
         }
