ae6122c6605576859f9019e55f581c2de758090b
Moshe <mmoshikoo@gmail.com>
2018-02-08 22:18:44 +0200

    Fixed Proxy

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace DoFactory.GangOfFour.Proxy.RealWorld
+namespace MosheBinieli.Proxy
 {
     class MainApp
     {
         static void Main()
         {
             Console.WriteLine("Proxy:");
+
             // assuming that the user selects a folder that has 3 images	
             //create the 3 images 	
             IImage highResolutionImage1 = new ImageProxy("sample/veryHighResPhoto1.jpeg");
@@ -16,20 +17,17 @@ namespace DoFactory.GangOfFour.Proxy.RealWorld
             // assume that the user clicks on Image one item in a list
             // this would cause the program to call showImage() for that image only
             // note that in this case only image one was loaded into memory
+
             highResolutionImage1.showImage();
             Console.WriteLine("\nNot proxy:");
+
             // consider using the high resolution image object directly
             IImage highResolutionImageNoProxy1 = new HighResolutionImage("sample/veryHighResPhoto1.jpeg");
             IImage highResolutionImageNoProxy2 = new HighResolutionImage("sample/veryHighResPhoto2.jpeg");
             IImage highResolutionImageBoProxy3 = new HighResolutionImage("sample/veryHighResPhoto3.jpeg");
 
-
             // assume that the user selects image two item from images list
             highResolutionImageNoProxy2.showImage();
-
-            // note that in this case all images have been loaded into memory 
-            // and not all have been actually displayed
-            // this is a waste of memory resources
         }
     }
 }
